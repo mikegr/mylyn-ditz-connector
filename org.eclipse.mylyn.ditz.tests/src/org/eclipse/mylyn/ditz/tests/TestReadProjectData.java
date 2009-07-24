e41ae9fb5d9fccabcce60bb77080852936756cf5
Michael Greifeneder <mikegr@gmx.net>
2009-07-24 09:56:06 +0200

    laptop work

---
+++ org.eclipse.mylyn.ditz.tests/src/org/eclipse/mylyn/ditz/tests/TestReadProjectData.java
@@ -0,0 +1,20 @@
+package org.eclipse.mylyn.ditz.tests;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class TestReadProjectData {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	public void testReadConfig() {
+
+	}
+}
